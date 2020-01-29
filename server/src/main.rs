76406d7cb1d4751376e1902f8e9b0893c761088e
ken <rsy@MacBook-Pro-5.local>
2020-01-29 19:29:24 +0900

    オーとリロードにしたみたい

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,9 @@
 use actix_web::{web, get, App, HttpResponse, HttpServer, Responder};
+use listenfd::ListenFd;
 
+#[get("/index")]
 async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    HttpResponse::Ok().body(include_str!("client/dst/index.html"))
 }
 
 async fn index2() -> impl Responder {
@@ -15,13 +17,19 @@ async fn index3() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let mut listenfd = ListenFd::from_env();
+    let mut server =  HttpServer::new(|| {
         App::new()
             .service(index3)
             .route("/", web::get().to(index))
             .route("/again", web::get().to(index2))
-    })
-    .bind("127.0.0.1:8088")?
-    .run()
-    .await
+    });
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)?
+    } else {
+        server.bind("127.0.0.1:8080")?
+    };
+
+    server.run().await
 }
