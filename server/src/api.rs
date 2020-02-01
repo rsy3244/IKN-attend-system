abf5ed9829c77dcc549c64c750c0dc75468b826a
ken <rsy@MacBook-Pro-5.local>
2020-02-01 23:26:12 +0900

    students,student実装（例外処理はまだ）

--- server/src/api.rs
+++ server/src/api.rs
@@ -1,16 +1,18 @@
 use actix_web::{web, HttpResponse, Result};
 
 use super::person::*;
+use super::db::*;
+use super::diesel::prelude::*;
 
 
-pub async fn attend(info: web::Path<(usize,)>) -> Result<HttpResponse> {
+pub async fn attend(info: web::Path<(i32,)>) -> Result<HttpResponse> {
     Ok(HttpResponse::Ok().json(
             Person::new(info.0, "サザエさん".to_string(), Role::B4, State::Leave)
             .to_raw()
     ))
 }
 
-pub async fn leave(info: web::Path<(usize,)>) -> Result<HttpResponse> {
+pub async fn leave(info: web::Path<(i32,)>) -> Result<HttpResponse> {
     Ok(HttpResponse::Ok().json(
             Person::new(info.0, "サザエさん".to_string(), Role::B4, State::Leave)
             .to_raw()
@@ -18,18 +20,37 @@ pub async fn leave(info: web::Path<(usize,)>) -> Result<HttpResponse> {
 }
 
 pub async fn get_all() -> Result<HttpResponse> {
+    use super::schema::people::dsl::*;
+    let connection = super::db::establish_connection();
+    let results = people
+        .load::<PersonRaw>(&connection)
+        .expect("Error loading people");
     Ok(HttpResponse::Ok().json(
-            vec![
-                Person::new(1, "カツオ".to_string(), Role::Professor, State::Leave)
-                .to_raw()
-                ; 3
-            ]
+            results
     ))
 }
 
-pub async fn get_student(info: web::Path<(usize,)>) -> Result<HttpResponse> {
+pub async fn get_student(info: web::Path<(i32,)>) -> Result<HttpResponse> {
+    use super::schema::people::dsl::*;
+    let connection = super::db::establish_connection();
+    let results = people
+        .filter(id.eq(info.0))
+        .load::<PersonRaw>(&connection)
+        .expect("Error loading people");
     Ok(HttpResponse::Ok().json(
-            Person::new(info.0, "波平".to_string(), Role::M1, State::Attend)
-            .to_raw()
+        results.iter().next()
     ))
 }
+
+//pub async fn signup<'a>(info: web::Json<NewPerson<'a>>) -> Result<HttpResponse> {
+//    use super::schema::people;
+//    use super::schema::people::dsl::*;
+//    let connection = super::db::establish_connection();
+//    diesel::insert_into(people::table)
+//        .values(&info)
+//        .execute(connection)
+//        .expect("Error signup");
+//    Ok(HttpResponse::Ok())
+//}
+
+
--- server/src/person.rs
+++ server/src/person.rs
@@ -1,22 +1,60 @@
 use serde::{Deserialize, Serialize};
+use super::schema::people;
 
-#[derive(Clone, Deserialize, Serialize)]
+#[derive(Clone, Deserialize, Serialize, Queryable)]
 pub struct PersonRaw {
-    id : usize,
-    username : String,
-    role : String,
-    state : usize,
+    pub id: i32,
+    pub username: String,
+    pub role: String,
+    pub state: i32,
+}
+
+impl PersonRaw {
+    fn to_person(&self) -> Person {
+        Person {
+            id: self.id,
+            username: self.username.clone(),
+            role: match self.role.as_str() {
+                "B1" => Role::B1,
+                "B2" => Role::B2,
+                "B3" => Role::B3,
+                "B4" => Role::B4,
+                "M1" => Role::M1,
+                "M2" => Role::M2,
+                "D1" => Role::D1,
+                "D2" => Role::D2,
+                "D3" => Role::D3,
+                "Secretary" => Role::Secretary,
+                "Professor" => Role::Professor,
+                "AssociateProfessor" => Role::AssociateProfessor,
+                "ResearchAssociate" => Role::ResearchAssociate,
+                _ => panic!("not implemented!"),
+            },
+            state: match self.state {
+                0 => State::Leave,
+                1 => State::Attend,
+                _ => panic!("not implemented!"),
+            }
+        }
+    }
+}
+
+#[derive(Insertable)]
+#[table_name="people"]
+pub struct NewPerson<'a> {
+    pub username: &'a str,
+    pub role: &'a str,
 }
 
 pub struct Person {
-    id : usize,
-    username : String,
-    role : Role,
-    state : State,
+    id: i32,
+    username: String,
+    role: Role,
+    state: State,
 }
 
 impl Person {
-    pub fn new(id: usize, username: String, role: Role, state: State) -> Person {
+    pub fn new(id: i32, username: String, role: Role, state: State) -> Person {
         Person { id, username, role, state }
     }
 
@@ -25,7 +63,7 @@ impl Person {
             id: self.id,
             username: self.username.clone(),
             role: self.role.to_string(),
-            state: self.state.to_usize(),
+            state: self.state.to_i32(),
         }
     }
 }
@@ -39,7 +77,7 @@ pub enum State {
 
 #[allow(unreachable_patterns)]
 impl State {
-    pub fn to_usize(&self) -> usize {
+    pub fn to_i32(&self) -> i32 {
         use State::*;
         match self {
             Attend => 1,
@@ -67,23 +105,29 @@ pub enum Role {
     ResearchAssociate,
 }
 
-#[allow(unreachable_patterns)]
-impl ToString for Role {
-    fn to_string(&self) -> String {
-        match self {
-            Role::B1 => "B1".to_string(),
-            Role::B2 => "B2".to_string(),
-            Role::B3 => "B3".to_string(),
-            Role::B4 => "B4".to_string(),
-            Role::M1 => "M1".to_string(),
-            Role::M2 => "M2".to_string(),
-            Role::D1 => "D1".to_string(),
-            Role::D2 => "D2".to_string(),
-            Role::D3 => "D3".to_string(),
-            Role::Professor => "Professor".to_string(),
-            Role::AssociateProfessor => "AssociateProfessor".to_string(),
-            Role::ResearchAssociate => "ResearchAssociate".to_string(),
-            _ => panic!("not implemented!")
-        }
+//#[allow(unreachable_patterns)]
+//impl ToString for Role {
+//    fn to_string(&self) -> String {
+//        match self {
+//            Role::B1 => "B1".to_string(),
+//            Role::B2 => "B2".to_string(),
+//            Role::B3 => "B3".to_string(),
+//            Role::B4 => "B4".to_string(),
+//            Role::M1 => "M1".to_string(),
+//            Role::M2 => "M2".to_string(),
+//            Role::D1 => "D1".to_string(),
+//            Role::D2 => "D2".to_string(),
+//            Role::D3 => "D3".to_string(),
+//            Role::Professor => "Professor".to_string(),
+//            Role::AssociateProfessor => "AssociateProfessor".to_string(),
+//            Role::ResearchAssociate => "ResearchAssociate".to_string(),
+//            _ => panic!("not implemented!")
+//        }
+//    }
+//}
+
+impl std::fmt::Display for Role {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.to_string())
     }
 }
