970d6554ea0e746fdfc122fcef4d298b0467ba55
rsy <hoge@huga.com>
2021-02-10 14:40:54 +0900

    fix attend()

--- server/src/api.rs
+++ server/src/api.rs
@@ -4,51 +4,43 @@ use super::person::*;
 
 use super::db::DbPool;
 
-pub async fn attend(
-    pool: web::Data<DbPool>,
-    info: web::Path<(i32,)>
-) -> Result<HttpResponse> {
+pub async fn attend(pool: web::Data<DbPool>, info: web::Path<(i32,)>) -> Result<HttpResponse> {
     let connection = pool.get().expect("couldn't get db connection from pool");
     let input: i32 = info.0;
-    let result = web::block(move || super::db::update_user_status(input, State::Leave, &connection))
-        .await
-        .map_err(|e| {
-            eprintln!("{}", e);
-            HttpResponse::InternalServerError().finish()
-        })?;
+    let result =
+        web::block(move || super::db::update_user_status(input, State::Attend, &connection))
+            .await
+            .map_err(|e| {
+                eprintln!("{}", e);
+                HttpResponse::InternalServerError().finish()
+            })?;
     if let Some(result) = result {
         Ok(HttpResponse::Ok().json(result))
     } else {
-        let res = HttpResponse::NotFound()
-            .body(format!("No user found with id: {}", input));
+        let res = HttpResponse::NotFound().body(format!("No user found with id: {}", input));
         Ok(res)
     }
 }
 
-pub async fn leave(
-    pool: web::Data<DbPool>,
-    info: web::Path<(i32,)>
-) -> Result<HttpResponse> {
+pub async fn leave(pool: web::Data<DbPool>, info: web::Path<(i32,)>) -> Result<HttpResponse> {
     let connection = pool.get().expect("couldn't get db connection from pool");
     let input: i32 = info.0;
-    let result = web::block(move || super::db::update_user_status(input, State::Leave, &connection))
-        .await
-        .map_err(|e| {
-            eprintln!("{}", e);
-            HttpResponse::InternalServerError().finish()
-        })?;
+    let result =
+        web::block(move || super::db::update_user_status(input, State::Leave, &connection))
+            .await
+            .map_err(|e| {
+                eprintln!("{}", e);
+                HttpResponse::InternalServerError().finish()
+            })?;
     if let Some(result) = result {
         Ok(HttpResponse::Ok().json(result))
     } else {
-        let res = HttpResponse::NotFound()
-            .body(format!("No user found with id: {}", input));
+        let res = HttpResponse::NotFound().body(format!("No user found with id: {}", input));
         Ok(res)
     }
 }
 
-pub async fn get_all(
-    pool: web::Data<DbPool>,
-) -> Result<HttpResponse> {
+pub async fn get_all(pool: web::Data<DbPool>) -> Result<HttpResponse> {
     let connection = pool.get().expect("couldn't get db connection from pool");
     let ret = web::block(move || super::db::list_users(&connection))
         .await
@@ -59,10 +51,7 @@ pub async fn get_all(
     Ok(HttpResponse::Ok().json(ret))
 }
 
-pub async fn get_student(
-    pool: web::Data<DbPool>,
-    info: web::Path<(i32,)>
-) -> Result<HttpResponse> {
+pub async fn get_student(pool: web::Data<DbPool>, info: web::Path<(i32,)>) -> Result<HttpResponse> {
     let connection = pool.get().expect("couldn't get db connection from pool");
     let input: i32 = info.0;
     let person = web::block(move || super::db::find_user_by_id(input, &connection))
@@ -75,8 +64,7 @@ pub async fn get_student(
     if let Some(person) = person {
         Ok(HttpResponse::Ok().json(person))
     } else {
-        let res = HttpResponse::NotFound()
-            .body(format!("No user found with id: {}", input));
+        let res = HttpResponse::NotFound().body(format!("No user found with id: {}", input));
         Ok(res)
     }
 }
